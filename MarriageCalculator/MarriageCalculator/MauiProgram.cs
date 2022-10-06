ddfd81bbb135d8226818210da139fbbc71990d54
sanjeebjobs <105891963+sanjeebjobs@users.noreply.github.com>
2022-10-07 00:31:38 +0100

    added buttons to landing page

--- MarriageCalculator/MarriageCalculator/MauiProgram.cs
+++ MarriageCalculator/MarriageCalculator/MauiProgram.cs
@@ -1,18 +1,22 @@
-﻿namespace MarriageCalculator;
+﻿using CommunityToolkit.Maui;
+
+namespace MarriageCalculator;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-		return builder.Build();
-	}
+        builder.Services.AddSingleton<MainPage>();
+        return builder.Build();
+    }
 }
